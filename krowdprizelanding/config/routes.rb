a2cb8b6ccc6514887001969345b728de2d14364c
Javier Noris <norisjavier@gmail.com>
2014-07-05 15:35:49 -0700

    krowdprize landing page done

--- krowdprizelanding/config/routes.rb
+++ krowdprizelanding/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'pages#landing'
+
+  match '/home',    to: 'pages#home',    via: 'get'
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
